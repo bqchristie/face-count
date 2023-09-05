b653dff05b9aadb6ee2fa78b84b83b60954eadb9
Bruce Christie <bqchristie@gmail.com>
2023-09-05 17:18:24 -0400

    a couple of style tweaks and some instruction on the login page

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,15 @@
 <template>
   <h1>Facecount Login</h1>
   <div class="login">
-    <label for="username">Username</label>
+    <label for="username">Please enter your username.</label>
     <input name="username" v-model="username" type="text" />
-    <button type="submit" @click="onClickLogin()">Login</button>
+    <p>
+      NOTE: This is just a demo. All usernames will work. If the user does not exist a new one will
+      be created. <strong>To login as admin use the username: "admin".</strong>
+    </p>
+    <div class="buttons">
+      <button type="submit" @click="onClickLogin()">Login</button>
+    </div>
   </div>
 </template>
 <script>
@@ -21,3 +27,23 @@ export default {
   }
 }
 </script>
+<style scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+}
+.buttons {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 1em;
+}
+p {
+  font-size: 0.8rem;
+  color: var(--vt-c-text-light-1);
+  width: 270px;
+  margin: auto;
+}
+p strong {
+  font-weight: bold;
+}
+</style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -25,7 +25,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: #0b58a4;
   display: flex;
   justify-content: center;
   align-items: center;
